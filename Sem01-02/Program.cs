dd8b8e0486724c773bb7c7bb59ba62fbefd2183a
DenSmith1 <ntreid80@gmail.com>
2022-04-25 12:42:23 +0400

    Семинар задание 2

---
+++ Sem01-02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("input digit A ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("input digit B ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("input digit C ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+Console.Write("Max = ");
+Console.WriteLine(max);
