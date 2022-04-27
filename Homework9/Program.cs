2f59ff1f1389a3d42ce29846c83790fa76e13c2d
DenSmith1 <ntreid80@gmail.com>
2022-04-27 21:10:24 +0400

    Домашнее задание 9. Найти 3ю цифру числа

---
+++ Homework9/Program.cs
@@ -0,0 +1,24 @@
+﻿// Домашнее задание 9. Найти 3ю цифру числа
+Console.Write("Введите любое число: ");
+int Number = int.Parse(Console.ReadLine());
+if (Number < 99)
+{
+    Console.Write("Нет третьей цифры");
+}
+int BitDepth = 2;                               // начальная разрядность числа
+int NumberDepth = Number;
+while (NumberDepth / 10 > 9)                    // определяем разрядность числа
+{
+    NumberDepth = NumberDepth / 10;
+    BitDepth++;
+}
+// думаю проще сделать через степень (Pow), но раз мы ее не проходили, то вот такое решение:
+int count = 1;
+int Depth = 1;
+while (count < BitDepth - 1)
+{
+    Depth = Depth * 10;                         // глубина разрядности для расчета 3 цифры
+    count++;
+}
+Console.Write("Третья цифра числа: ");
+Console.WriteLine(Number % Depth / (Depth / 10));
