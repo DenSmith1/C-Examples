2084817a153173dcb403b35b1dd63eab546784b6
DenSmith1 <ntreid80@gmail.com>
2022-04-27 19:08:46 +0400

    Домашнее задание 4. Ряд четных чисел

---
+++ Homework4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Домашнее задание 4. Показать четные числа
+Console.Write("Введите любое число больше 1: ");
+int number1 = int.Parse(Console.ReadLine());
+int count = 1;
+while (number1 < 2)
+{
+    Console.Write("Введите любое число больше 1: ");
+    number1 = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Ряд четных чисел: ");
+while (count <= number1)
+{
+    if (count%2 == 0)
+    {
+        Console.Write (" "+count);
+    }
+    count++;
+}
