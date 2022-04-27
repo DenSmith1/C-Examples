afe872d439f404328aaf2e7abf83e814cadc772d
DenSmith1 <ntreid80@gmail.com>
2022-04-27 19:40:58 +0400

    Домашнее задание 5. Вторая цифра трехзначного числа

---
+++ Homework5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Домашнее задание 5. Показать вторую цифру трехзначного числа
+Console.Write("Введите любое трехзначное число: ");
+int number1 = int.Parse(Console.ReadLine());
+while (number1 < 100 || number1 > 999)
+{
+    Console.Write("ТРЕХЗНАЧНОЕ (THREE-DIGIT, DREISTELLIG, TROIS CHIFFRES): ");
+    number1 = int.Parse(Console.ReadLine());
+}
+int SecondDigit = (number1 % 100) / 10;
+Console.WriteLine("Вторая цифра: " + SecondDigit);
