1dded99821949648b31270abbbe1084aaccadab0
DenSmith1 <ntreid80@gmail.com>
2022-04-27 20:19:58 +0400

    Домашнее задание 6. Определение максимальной цифры в числе

---
+++ Homework6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашнее задание 6. Сравнение чисел в диапазоне
+Console.Write("Введите число в интервале [10:99]: ");
+int number1 = int.Parse(Console.ReadLine());
+while (number1 < 10 || number1 > 99)
+{
+    Console.Write("Попробуйте еще раз. Интервал 10 - 99: ");
+    number1 = int.Parse(Console.ReadLine());
+}
+int FirstDigit = number1 / 10;
+int SecondDigit = number1 % 10;
+if (FirstDigit >= SecondDigit)
+{
+    Console.WriteLine(FirstDigit);
+}
+else
+    Console.WriteLine(SecondDigit);
