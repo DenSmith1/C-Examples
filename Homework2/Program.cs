4a91a2429f4a3dd932d48a7aa575f4dafe5fcc3b
DenSmith1 <ntreid80@gmail.com>
2022-04-27 18:50:41 +0400

    Домашнее задание 2. дни недели

---
+++ Homework2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Домашнее задание 2. Дни недели
+Console.Write("Введите номер дня недели (1-7): ");
+int number1 = int.Parse(Console.ReadLine());
+while (number1 > 7 || number1 < 1)
+{
+    Console.Write("Вы ввели неверный номер недели. Повторите попытку: ");
+    number1 = int.Parse(Console.ReadLine());
+}
+if (number1 == 1)
+{
+    Console.Write("ПОНЕДЕЛЬНИК");
+}
+if (number1 == 2)
+{
+    Console.Write("ВТОРНИК");
+}
+if (number1 == 3)
+{
+    Console.Write("СРЕДА");
+}
+if (number1 == 4)
+{
+    Console.Write("ЧЕТВЕРГ");
+}
+if (number1 == 5)
+{
+    Console.Write("ПЯТНИЦА");
+}
+if (number1 == 6)
+{
+    Console.Write("СУББОТА");
+}
+if (number1 == 7)
+{
+    Console.Write("ВОСКРЕСЕНЬЕ");
+}
