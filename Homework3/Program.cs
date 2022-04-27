86babbd7a8a627e892844c26c082efb926178446
DenSmith1 <ntreid80@gmail.com>
2022-04-27 18:55:52 +0400

    Домашнее задание 3. Четные числа

---
+++ Homework3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Домашнее задание 3. Четные числа
+Console.Write("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+if (number1%2 == 1)
+{
+    Console.Write("Это число нечетное");
+}
+else
+    Console.Write("Это число четное");
