6c963399b36976473201551d0e75679f7eaae217
DenSmith1 <ntreid80@gmail.com>
2022-04-27 18:09:25 +0400

    Текст и переменные в одну строку

---
+++ Sem01-03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("What is your name? ");
+string name = Console.ReadLine();
+Console.Write("How old are you? ");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Your name is {0}. Your are {1} years old.",name,age);
+
+Console.Write("What is your name? ");
+string name = Console.ReadLine();
+Console.Write("How old are you? ");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine($"Your name is {name}. Your are {age} years old.");
