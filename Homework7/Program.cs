5e2693f2aa0c673b4401711f1483ac69f59dd5d1
DenSmith1 <ntreid80@gmail.com>
2022-04-27 20:27:35 +0400

    Домашнее задание 7. Удаление второй цифры в числе

---
+++ Homework7/Program.cs
@@ -0,0 +1,12 @@
+﻿// Домашнее задание 7. Удалить вторую цифру трехзначного числа
+Console.Write("Введите любое трехзначное число: ");
+int number1 = int.Parse(Console.ReadLine());
+while (number1 < 100 || number1 > 999)
+{
+    Console.Write("ТРЕХЗНАЧНОЕ (THREE-DIGIT, DREISTELLIG, TROIS CHIFFRES): ");
+    number1 = int.Parse(Console.ReadLine());
+}
+int FirstDigit = number1 / 100;
+int ThirdDigit = number1 % 10;
+int NewNumber = FirstDigit*10 + ThirdDigit;
+Console.WriteLine (NewNumber);
