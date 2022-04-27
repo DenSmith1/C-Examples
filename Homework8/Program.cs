65de8e880893d63f3c57131434fea307882c9d03
DenSmith1 <ntreid80@gmail.com>
2022-04-27 20:38:49 +0400

    Домашнее задание 8. Кратность числа

---
+++ Homework8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Домашнее задание 8. Кратность числа
+Console.Write("Введите любое число: ");
+int Number = int.Parse(Console.ReadLine());
+int GivenNumber = 7;                                 // заданное число
+if ((Number - Number/GivenNumber * GivenNumber) == 0)
+{
+    Console.Write("Ваше число кратно "+GivenNumber);
+}
+else    
+    Console.Write("Ваше число не кратно "+GivenNumber);
